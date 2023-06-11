38f6b537247359773a6388bba031e76810294efd
Brian Dunne <dunnebrian2012@gmail.com>
2023-06-11 11:18:08 +0100

    brute force prime checker

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <cstdio>
+#include <cmath>
+
+bool isPrime(int a){
+    const int cutoff = sqrt(a);
+    bool isPrime= true;
+    size_t i;
+    for(i=2; i<=cutoff;i++){
+        if(a%i==0){
+            isPrime= false;
+        }
+    }
+    return isPrime;
+
+}
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;//
 
-    printf( "Hello, World!");
+    printf( "Hello, World! here is a thing:%d",isPrime(21));
 
 
     return 0;
 }
+
+
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Brian Dunne on 11/06/2023.
+//
+
+#ifndef UNTITLED_MAIN_H
+#define UNTITLED_MAIN_H
+
+#endif //UNTITLED_MAIN_H
